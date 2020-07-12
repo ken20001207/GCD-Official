14604a8eec0b7bb393fa93ca24ad7f26d5d724a5
Yuanlin Lin <im.yuanlinlin@gmail.com>
2020-07-13 00:41:03 +0800

    add footer

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "../Styles/Footer.less";
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div className="footer">
+                <p>Copyright © 2020 All rights reserved.</p>
+            </div>
+        );
+    }
+}
