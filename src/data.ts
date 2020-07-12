8d2496e42e8d2e70b0ee43ac65b947827344a01b
Yuanlin Lin <im.yuanlinlin@gmail.com>
2020-07-13 00:42:37 +0800

    add config file for highlighted projects

---
+++ src/data.ts
@@ -0,0 +1,38 @@
+export const HightLights = [
+    {
+        projectID: "Project1",
+        description: "Highlight1",
+        class: "Development",
+        coverPhoto: "1.png",
+    },
+    {
+        projectID: "Project2",
+        description: "Highlight2",
+        class: "Development",
+        coverPhoto: "2.png",
+    },
+    {
+        projectID: "Project3",
+        description: "Highlight3",
+        class: "Development",
+        coverPhoto: "3.png",
+    },
+    {
+        projectID: "Project4",
+        description: "Highlight4",
+        class: "Development",
+        coverPhoto: "4.png",
+    },
+    {
+        projectID: "Project5",
+        description: "Highlight5",
+        class: "Development",
+        coverPhoto: "5.png",
+    },
+    {
+        projectID: "Project6",
+        description: "Highlight6",
+        class: "Development",
+        coverPhoto: "6.png",
+    },
+];
