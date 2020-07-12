80657db30b7299fee84a0a1aeba9c6a283b66c65
ken20001207 <varedx@gmail.com>
2020-07-12 17:28:06 +0800

    add empty page components

---
+++ src/Pages/Contact.tsx
@@ -0,0 +1,8 @@
+import { Component } from "react";
+import React from "react";
+
+export default class Contact extends Component {
+    render() {
+        return <div />;
+    }
+}
---
+++ src/Pages/Projects.tsx
@@ -0,0 +1,8 @@
+import { Component } from "react";
+import React from "react";
+
+export default class Projects extends Component {
+    render() {
+        return <div />;
+    }
+}
