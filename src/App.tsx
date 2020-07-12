a58cbfc151028cce6d95368f84c07409785fc17e
Yuanlin Lin <im.yuanlinlin@gmail.com>
2020-07-13 00:41:37 +0800

    transfer history prop for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,34 @@
 import React, { Component } from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import "./Styles/App.less";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Contact from "./Pages/Contact";
 import Landing from "./Pages/Landing";
 import Projects from "./Pages/Projects";
-import Contact from "./Pages/Contact";
+import "./Styles/App.less";
 
 class App extends Component {
     render() {
         return (
             <BrowserRouter>
                 <Switch>
-                    <Route path="/" render={() => <Landing />} />
-                    <Route path="/projects" render={() => <Projects />} />
-                    <Route path="/contact" render={() => <Contact />} />
+                    <Route
+                        exact
+                        path="/"
+                        render={(history) => (
+                            <Landing history={history} />
+                        )}
+                    />
+                    <Route
+                        path="/projects"
+                        render={(history) => (
+                            <Projects history={history} />
+                        )}
+                    />
+                    <Route
+                        path="/contact"
+                        render={(history) => (
+                            <Contact history={history} />
+                        )}
+                    />
                 </Switch>
             </BrowserRouter>
         );
